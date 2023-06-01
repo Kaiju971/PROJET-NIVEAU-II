6d336fd163409df45891836bbd39fb1c40477ab8
Kaiju971 <129183625+Kaiju971@users.noreply.github.com>
2023-06-01 13:07:28 +0200

    V2

---
+++ pages/contact.js
@@ -0,0 +1,20 @@
+// let containerContacts = document.querySelector(".container-contact");
+
+// containerContacts.innerHTML = containerContacts.innerHTML +`<form>
+// <h5>Une question ? un problème ? N’hésiter pas à nous envoyer un message </h5>
+//   <label for="nom">nom:</label>
+//   <input type="text" id="nom" name="nom" value="" class = "input"/>
+//   <br />
+//   <label for="email">email:</label>
+//   <input type="text" id="email" name="email" value="" class = "input"/>
+//   <br />
+//   <label for="sujet">sujet:</label>
+//   <input type="text" id="sujet" name="sujet" value="" class = "input"/>
+//   <br />
+//   <label for="message">message:</label>
+//   <br />
+  
+//   <textarea name="message" id="" cols="60" rows="3"></textarea>
+//   <br />
+//   <button class="button">Envoyer mon message</button>
+// </form>`
