1d7b3f2bccc11b6eee8920d600ea49df9eb6e419
Kaiju971 <129183625+Kaiju971@users.noreply.github.com>
2023-06-02 17:07:52 +0200

    V4.4

--- pages/services.js
+++ pages/services.js
@@ -1,8 +1,8 @@
 var captionSel = document.querySelectorAll('.caption');
 
 for (let i = 0; i < captionSel.length; i++) {
-  let image = captionSel[i].querySelector(":scope > .caption-image");
-  let text = captionSel[i].querySelector(":scope > .caption-text");
+  let image1 = captionSel[i].querySelector(":scope > .caption-image1");
+  let image2 = captionSel[i].querySelector(":scope > .caption-image2");
   text.style.width = image.clientWidth - 20 + "px";
   captionSel[i].style.height = image.clientHeight + "px";
 }
